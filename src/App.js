d90ee7b27de8cdac681fe5a90bab6a48dc645fd9
GH-Mina <mona.ghaffour0@gmail.com>
2023-10-08 20:02:42 +0100

    Wrap in Layout component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Router, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainNavigation from "./MainNavigation";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <MainNavigation />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
