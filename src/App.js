c38fe0080430b2b3c95e8b6872e4d5011116a712
GH-Mina <mona.ghaffour0@gmail.com>
2023-10-08 19:39:52 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Router, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
