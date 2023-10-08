ad2a5f1b22fff28b896184b02be4e12f664cc25a
GH-Mina <mona.ghaffour0@gmail.com>
2023-10-08 20:38:40 +0100

    Add meetup form UI

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MainNavigation from "./MainNavigation";
+import classes from './Layout.module.css'
 
 const Layout = ({ children }) => {
   return (
     <div>
       <MainNavigation />
-      <main>{children}</main>
+      <main className={classes.main}>{children}</main>
     </div>
   );
 };
