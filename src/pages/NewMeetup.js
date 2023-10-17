775c88795460097f2b3ea02ec942a38c0fd10202
GH-Mina <mona.ghaffour0@gmail.com>
2023-10-17 21:56:13 +0100

    Save meetup in firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,12 +2,23 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const onAddNewMeetup = (meetup) => {};
+  const addNewMeetupHandler = (meetup) => {
+    fetch(
+      "https://react-onboarding-da225-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetup),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      }
+    );
+  };
 
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddNewMeetup={onAddNewMeetup} />
+      <NewMeetupForm onAddNewMeetup={addNewMeetupHandler} />
     </section>
   );
 };
