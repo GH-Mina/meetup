3e29647384c7677f172b33a7db26afcfb029273c
GH-Mina <mona.ghaffour0@gmail.com>
2023-10-17 21:46:00 +0100

    Handle add new meetup

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "../wrapper/Card";
 import classes from "./NewMeetupForm.module.css";
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
+  const meetupTitle = useRef();
+  const meetupImage = useRef();
+  const meetupAddress = useRef();
+  const meetupDescriptioon = useRef();
+
+  const onConfirmClick = (event) => {
+    event.preventDefault();
+    const meetup = {
+      title: meetupTitle.current.value,
+      image: meetupImage.current.value,
+      address: meetupAddress.current.value,
+      description: meetupDescriptioon.current.value,
+    };
+    props.onAddNewMeetup(meetup);
+  };
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={onConfirmClick}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={meetupTitle} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={meetupImage} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={meetupAddress} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-         <textarea id="description" required rows='5'></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={meetupDescriptioon}
+          />
         </div>
         <div className={classes.actions}>
-            <button>Add Meetup</button>
+          <button>Add Meetup</button>
         </div>
       </form>
     </Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,12 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const onAddNewMeetup = (meetup) => {};
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddNewMeetup={onAddNewMeetup} />
     </section>
   );
 };
